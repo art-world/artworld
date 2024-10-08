9651cf1e07ab72e1ccd1969e6b3842eef2d4a80e
artworldtheory <arthur.hopewell@gmail.com>
2024-10-08 16:18:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ function init() {
     // Load model
     const loader = new THREE.GLTFLoader();
     loader.load(
-        'assets/model/Walkman buttons screen geo.gltf',
+        'assets/model/Buttons2.gltf',
         function(gltf) {
             console.log('Model loaded successfully.');
             model = gltf.scene;
