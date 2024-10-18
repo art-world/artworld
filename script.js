41032951c14aec6bd69afdcfdea7d3cf1b6d7680
artworldtheory <arthur.hopewell@gmail.com>
2024-10-18 21:30:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -155,7 +155,7 @@ function createVideoTexture() {
 
         // Scale the video down
         videoTexture.repeat.set(4.1, 4.1); // Scale it down
-        videoTexture.offset.set(-1.01, -1.05); // Center the texture on the object
+        videoTexture.offset.set(-1.05, -1.05); // Center the texture on the object
     });
 }
 
